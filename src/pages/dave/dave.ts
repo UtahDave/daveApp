fb3fc298eee7791d376a5b61ace55f60dd1a9137
David Boucha <boucha@gmail.com>
2018-09-27 20:26:55 -0600

    add a dave tab

---
+++ src/pages/dave/dave.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-dave',
+  templateUrl: 'dave.html'
+})
+export class DavePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
